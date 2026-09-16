3cb6ce82b3672d39e4cabb875b58bc156e399165
Senaton <tosin4real88@yahoo.com>
2026-09-16 12:21:20 +0200

    New  docker

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 from tools.tavily_tool import tavily_search
 
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 # res= tavily_search("Best hotels in Paris")
 # print(res)
 
-res = search_flights("plan a 2 weeks slovenia trip from Lagos, Nigeria")
-print(res)
+# res = search_flights("plan a 2 weeks slovenia trip from Lagos, Nigeria")
+# print(res)
+
+user_input=input("Enter your travel query: ")
+response = run_travel_agent(user_input=user_input,thread_id="test_user")
+
+
+print("\nFlight Results:\n")
+print(response["answer"])
